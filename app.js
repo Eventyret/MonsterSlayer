2cf5f0cd0f06a5a91e018390cbf3b4848913a0fb
Simen Daehlin <simen.dehlin@gmail.com>
2018-04-14 00:15:30 +0100

    Implemented Attacks and special attacks

--- app.js
+++ app.js
@@ -10,6 +10,53 @@ new Vue({
 			this.gameIsRunning = true;
 			this.playerHealth = 100;
 			this.monsterHealth = 100;
+		},
+		attack: function() {
+			this.monsterHealth -= this.calculateDamage(3, 10)
+			if (this.checkWinner()) { 
+				return;
+			}
+			
+			this.monsterAttacks();
+		},
+		specialAttack: function() {
+			this.monsterHealth -= this.calculateDamage(10, 20);
+			if(this.checkWinner()) {
+				return
+			}
+			this.monsterAttacks()
+		},
+		heal: function() {
+			
+		},
+		giveUp: function(){
+			
+		},
+		monsterAttacks: function() {
+			this.playerHealth -= this.calculateDamage(5, 12);
+			this.checkWinner();
+		},
+
+		calculateDamage: function(min, max) {
+			return Math.max(Math.floor(Math.random() * max) + 1, min)
+		},
+		checkWinner() {
+			if (this.monsterHealth <= 0) {
+				if(confirm("You Win - Fatality. New Game?")) {
+					this.startGame();
+				} else {
+					this.gameIsRunning = false;
+				}
+				return true;
+			} else if (this.playerHealth <= 0) {
+				if(confirm("You Loose! New Game?")) {
+					this.startGame();
+				} else {
+					this.gameIsRunning = false;
+				}
+				return true;
+			}
+			return false;
 		}
 	}
 })
